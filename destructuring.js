029ad7460761a9c6a24e233fc1e9e61bd0ed5cee
Mohammod Ali <mohammodali651@gmail.com>
2022-08-26 23:41:53 +0600

    destructuring function added

---
+++ destructuring.js
@@ -0,0 +1,17 @@
+const fish = {
+    name: 'elish',
+    age: 2,
+    area: 'nodi'
+}
+
+const {age, name} = {name: 'Almas', age: '56', profession: 'makeup artist'}
+console.log(age);
+
+const {area} = fish
+console.log(area)
+
+// Array destructuring
+const [first, second] = [22, 44, 32, 55]
+console.log(first, second);
+
+const
